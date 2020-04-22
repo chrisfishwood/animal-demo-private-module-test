5a73826f228d3b9356c1fcb2c99167634e9f7bea
Chris Steinmeyer <csteinmeyer@gmail.com>
2020-04-21 23:44:36 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "animal" {
   name = "foo"
-  source  = "localterraform.com/ChrisCorp/animal/demo"
+  source  = "https://ptfe-kf6uwmup.ptfedev.com/ChrisCorp/animal/demo"
   version = "1.0.0"
 }
