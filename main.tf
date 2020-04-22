4b2ce7aaccbc5aff0de23d74431fbc27fe3c3c50
Chris Steinmeyer <csteinmeyer@gmail.com>
2020-04-21 23:47:40 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "animal" {
   name = "foo"
-  source  = "https://ptfe-kf6uwmup.ptfedev.com/ChrisCorp/animal/demo"
+  source  = "ptfe-kf6uwmup.ptfedev.com/ChrisCorp/animal/demo"
   version = "1.0.0"
 }
